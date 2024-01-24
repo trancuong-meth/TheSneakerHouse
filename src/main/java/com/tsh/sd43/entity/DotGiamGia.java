d1cbe5214cf21461bd6ed867a9e83c54d6cee5d0
trancuong-meth <ttrancuongg2003@gmail.com>
2024-01-24 13:29:45 +0700

    trạng thái entity

---
+++ src/main/java/com/tsh/sd43/entity/DotGiamGia.java
@@ -0,0 +1,41 @@
+package com.tsh.sd43.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "DotGiamGia")
+public class DotGiamGia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id")
+    private Long id;
+
+    @Column(name = "Ten")
+    private String ten;
+
+    @Column(name = "GiaTri")
+    private Integer giaTri;
+
+    @Column(name = "NgayBatDau")
+    private String ngayBatDau;
+
+    @Column(name = "NgayKetThuc")
+    private String ngayKetThuc;
+
+    @Column(name = "NgayTao")
+    private String ngayTao;
+
+    @Column(name = "NgayCapNhat")
+    private String ngayCapNhat;
+
+    @Column(name = "TrangThai")
+    private Integer trangThai;
+}
