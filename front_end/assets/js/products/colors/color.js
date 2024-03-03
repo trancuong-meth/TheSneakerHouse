426a48cd4bbf3e2b5b56e38b61632d12c494a761
thinhqp <tokhanhlinhph22707@fpt.edu.vn>
2024-03-03 16:11:38 +0700

    [ADD] add, update, search color, type back end

--- front_end/assets/js/products/colors/color.js
+++ front_end/assets/js/products/colors/color.js
@@ -5,23 +5,23 @@ main_app.controller("colorController", function ($scope, $http) {
     $scope.totalItems = 1;
     $scope.key = ""
     $scope.trang_thai = ""
-    $scope.brands = []
-    $scope.name_brand = "";
-    $scope.brand = {}
+    $scope.colors = []
+    $scope.name_color = "";
+    $scope.color = {}
   
     const loadData = function () {
-      $http.get('http://localhost:8080/brand/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + '&trang_thai=',)
+      $http.get('http://localhost:8080/color/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + '&trang_thai=',)
         .then(function (response) {
-          $scope.brands = response.data
+          $scope.colors = response.data
           $scope.totalItems = response.data.totalElements
           console.log($scope.totalItems)
         });
     }
   
     const fillter = function (key, trang_thai) {
-      $http.get('http://localhost:8080/brand/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + key + '&trang_thai=' + trang_thai,)
+      $http.get('http://localhost:8080/color/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + key + '&trang_thai=' + trang_thai,)
         .then(function (response) {
-          $scope.brands = response.data
+          $scope.colors = response.data
           $scope.totalItems = response.data.totalElements
           console.log($scope.totalItems)
         });
@@ -30,43 +30,43 @@ main_app.controller("colorController", function ($scope, $http) {
     loadData()
   
     $scope.pageChanged = function () {
-      $http.get('http://localhost:8080/brand/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + $scope.key + '&trang_thai=' + $scope.trang_thai,)
+      $http.get('http://localhost:8080/color/find-all-panigation?page=' + ($scope.currentPage - 1) + '&size=' + $scope.itemsPerPage + '&key=' + $scope.key + '&trang_thai=' + $scope.trang_thai,)
         .then(function (response) {
-          $scope.brands = response.data
+          $scope.colors = response.data
         });
     };
   
-    $scope.fillterByBrandKey = function () {
+    $scope.fillterBycolorKey = function () {
       fillter($scope.key, $scope.trang_thai)
     }
   
-    $scope.addBrand = function () {
-      var brandModal = document.querySelector("#brandModal")
-      var addModal = bootstrap.Modal.getOrCreateInstance(brandModal)
+    $scope.addcolor = function () {
+      var colorModal = document.querySelector("#colorModal")
+      var addModal = bootstrap.Modal.getOrCreateInstance(colorModal)
   
-      if ($scope.name_brand == "") {
-        toastr.error("Bạn phải nhập tên thương hiệu !!!");
+      if ($scope.name_color == "") {
+        toastr.error("Bạn phải nhập tên màu sắc !!!");
       } else {
-        axios.post('http://localhost:8080/brand/add?name=' + $scope.name_brand
+        axios.post('http://localhost:8080/color/add?name=' + $scope.name_color
         ).then(function (response) {
-          toastr.success("Bạn đã tạo thương hiệu thành công !!!");
+          toastr.success("Bạn đã tạo màu sắc thành công !!!");
           addModal.hide()
           loadData()
           $scope.reset()
         }).catch(function (error) {
-          toastr.error("Tên thương hiệu này đã tồn tại.Vui lòng nhập tên thương hiệu khác!!!");
+          toastr.error("Tên màu sắc này đã tồn tại.Vui lòng nhập tên màu sắc khác!!!");
         })
       }
     }
   
-    $scope.loadBrand = function (id) {
-      var brandUpdateModal = document.querySelector("#brandUpdateModal")
-      var modal = bootstrap.Modal.getOrCreateInstance(brandUpdateModal)
+    $scope.loadcolor = function (id) {
+      var colorUpdateModal = document.querySelector("#colorUpdateModal")
+      var modal = bootstrap.Modal.getOrCreateInstance(colorUpdateModal)
   
-      axios.get('http://localhost:8080/brand/get-brand/' + id).then(function (response) {
-        $scope.brand = response.data
-        console.log($scope.brand)
-        document.querySelector("#nameUpdateBrand").value = response.data.ten
+      axios.get('http://localhost:8080/color/get-color/' + id).then(function (response) {
+        $scope.color = response.data
+        console.log($scope.color)
+        document.querySelector("#nameUpdatecolor").value = response.data.ten
         modal.show()
       }).catch(function (error) {
         console.log(error)
@@ -74,12 +74,12 @@ main_app.controller("colorController", function ($scope, $http) {
       })
     }
   
-    $scope.updateBrand = function () {
-      var brandUpdateModal = document.querySelector("#brandUpdateModal")
-      var modal = bootstrap.Modal.getOrCreateInstance(brandUpdateModal)
+    $scope.updatecolor = function () {
+      var colorUpdateModal = document.querySelector("#colorUpdateModal")
+      var modal = bootstrap.Modal.getOrCreateInstance(colorUpdateModal)
   
-      console.log($scope.brand)
-      axios.put('http://localhost:8080/brand/update', $scope.brand)
+      console.log($scope.color)
+      axios.put('http://localhost:8080/color/update', $scope.color)
         .then(function (response) {
            modal.hide()
            toastr.success("Bạn đã thay đổi thông tin thành công !!!");
@@ -91,7 +91,7 @@ main_app.controller("colorController", function ($scope, $http) {
     }
   
     $scope.reset = function () {
-      $scope.name_brand = ""
+      $scope.name_color = ""
     }
   
   })
